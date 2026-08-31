ae5bcf39d561cb42ef70954778b59c4ff6429114
Sofiia Kozlova <159951127+SofiiaKozlova@users.noreply.github.com>
2026-08-31 17:52:51 +0300

    Update game

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,11 +3,17 @@ extends CharacterBody2D
 @export var speed := 200.0
 
 var carried_bubble = null
+var last_direction := "down"
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _ready():
 	add_to_group("player")
+	play_animation("idle_down")
 
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	var direction := Input.get_vector(
 		"ui_left",
 		"ui_right",
@@ -17,3 +23,31 @@ func _physics_process(delta):
 
 	velocity = direction * speed
 	move_and_slide()
+
+	# Визначаємо напрямок руху
+	if direction != Vector2.ZERO:
+
+		if abs(direction.x) > abs(direction.y):
+			if direction.x < 0:
+				last_direction = "left"
+				play_animation("walk_left")
+			else:
+				last_direction = "right"
+				play_animation("walk_right")
+		else:
+			if direction.y < 0:
+				last_direction = "up"
+				play_animation("walk_up")
+			else:
+				last_direction = "down"
+				play_animation("walk_down")
+
+	# Нічого не натиснуто
+	else:
+		play_animation("idle_" + last_direction)
+
+
+func play_animation(animation_name: String):
+	# Не перезапускаємо ту саму анімацію кожен кадр
+	if animated_sprite.animation != animation_name:
+		animated_sprite.play(animation_name)
